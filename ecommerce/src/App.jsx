c15d0d1af051c7e5763cab17f5e2c83e248a9c2c
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-02-27 23:38:36 +0530

    Hero section Completed

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Navbar/Hero'
+import Category from './components/Navbar/Category'
 
 export const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero/>
+      <Category/>
     </div>
   )
 }
